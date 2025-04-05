9747f887eebcd6eeaa3024ca38379d47e9fc7d14
Vishal <121089328+visak14@users.noreply.github.com>
2025-04-05 13:31:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://psquare-frontend-m8zs.onrender.com', 
   credentials: true,
 }));
 
